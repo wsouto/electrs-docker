c923120b2f90568e8f2d1fa5f810bd586b1d52ca
Walter Souto <wsouto@gmail.com>
2023-07-21 15:53:11 -0300

    chore: upgrade electrs to v0.10.0
    Besides the upgrade, a review with some changes was made.

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,31 @@
 # Build Electrs from Github Repository
-FROM debian:bookworm AS base
+
+FROM debian:bookworm-slim AS base
 
 LABEL maintainer="Walter Souto <wsouto@gmail.com>"
 
 RUN apt update -qy
 RUN apt install -qy librocksdb-dev
 
-FROM base as builder
+FROM base as build
 
-RUN apt install -qy git cargo clang cmake build-essential
+# RUN apt install -qy git cargo clang cmake build-essential
+RUN apt install -qy git cargo clang cmake
 
 WORKDIR /build
 
-ARG VERSION=v0.9.14
-ENV REPO=https://github.com/romanz/electrs.git
+ARG VERSION=v0.10.0
+ENV REPO="https://github.com/romanz/electrs.git"
 
 RUN git clone --branch $VERSION $REPO .
 RUN cargo build --release --bin electrs
 
 FROM base as deploy
 
-COPY --from=builder /build/target/release/electrs /bin/electrs
+COPY --from=build /build/target/release/electrs /bin/electrs
 
 EXPOSE 50001
 
-ENTRYPOINT ["electrs"]
-CMD ["--conf", "/data/config.toml"]
+# ENTRYPOINT ["electrs"]
+CMD ["electrs", "--conf", "/data/config.toml"]
 
--- docker-run.sh
+++ docker-run.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
+# This script is intended for test the image.
+# You need a bitcoin node to connect to.
+# Just edit the varioble bellow with the address of your node.
+# The crendentials to use can be found in the config.toml file.
+# After the test succeed you can delete the data directory.
+
+BITCOIN_NODE="bitcoin.local"
+
 docker run --rm \
 	--name electrs \
-	--env-file=.env \
-	-e ELECTRS_DB_DIR=${DB_DIR} \
-	-e ELECTRS_DAEMON_DIR=${DAEMON_DIR} \
-	-e ELECTRS_NETWORK=bitcoin \
-	-e ELECTRS_LOG_FILTERS=INFO \
-	-e ELECTRS_SERVER_BANNER="${BANNER}" \
-	-e ELECTRS_DAEMON_RPC_ADDR=${BTC_ADDR}:${BTC_RPC_PORT} \
-	-e ELECTRS_DAEMON_P2P_ADDR=${BTC_ADDR}:${BTC_P2P_PORT} \
-	-e ELECTRS_ELECTRUM_RPC_ADDR=${HOST_ADDR}:${HOST_PORT} \
-	-v ${HOST_BITCOIN_DIR}:${DAEMON_DIR}:ro \
-	-v ${HOST_ELECTRS_DIR}:/data \
-	-p ${HOST_PORT}:50001 \
-	${DOCKER_USER}/electrs:${TAG}
+	-e ELECTRS_DB_DIR="/data/db" \
+	-e ELECTRS_NETWORK="bitcoin" \
+	-e ELECTRS_LOG_FILTERS="INFO" \
+	-e ELECTRS_SERVER_BANNER="<<< ELECTRS TEST IMAGE >>>" \
+	-e ELECTRS_DAEMON_RPC_ADDR="${BITCOIN_NODE}:8332" \
+	-e ELECTRS_DAEMON_P2P_ADDR="${BITCOIN_NODE}:8333" \
+	-e ELECTRS_ELECTRUM_RPC_ADDR="0.0.0.0:50001" \
+	-v ./data:/data \
+	-v ./config.toml:/data/config.toml:ro \
+	-p 50001:50001 \
+	waltersouto/electrs:latest
